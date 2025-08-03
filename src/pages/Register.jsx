b3f676ec0aea3d60e3b1ffced8c70e42fe832ec6
blytheb <builtbyblythe@gmail.com>
2025-08-02 21:54:12 -1000

    Pages created

---
+++ src/pages/Register.jsx
@@ -0,0 +1,15 @@
+export default function Register() {
+    return <div>
+        <h2>Create Account</h2>
+        <form>
+            <div>
+                <label>Email:</label>
+                <input />
+            </div>
+            <div>
+                <label>Password:</label>
+                <input />
+            </div>
+        </form>
+    </div>
+}
