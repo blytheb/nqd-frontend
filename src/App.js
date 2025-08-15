e3ce68f23c9dde475409f3c06e8abeeed5be6613
blytheb <builtbyblythe@gmail.com>
2025-08-15 09:24:06 -1000

    updated authcontext to track login state

--- src/App.js
+++ src/App.js
@@ -1,35 +1,39 @@
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute';
+
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import ChildDash from './pages/ChildDash';
 
 import './App.css';
-import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route 
-          path='/home' 
-          element={
-            <ProtectedRoute>
-              <Home />
-            </ProtectedRoute>
-          } 
-        />
-        <Route
-          path='/dash'
-          element={
-            <ProtectedRoute>
-              <ChildDash />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route 
+            path='/home' 
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            } 
+          />
+          <Route
+            path='/dash'
+            element={
+              <ProtectedRoute>
+                <ChildDash />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </AuthProvider>
     </Router>
   );
 }
